f8ccbd039b8ccf60e1af18939c0469abb8efca0e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-08-19 21:08:15 +0900

    ✨Feat: 나루터 Grid

--- src/components/home/homeRoomList/HomeRoomList.tsx
+++ src/components/home/homeRoomList/HomeRoomList.tsx
@@ -1,106 +1,133 @@
-import { useEffect, useState } from 'react';
-import * as S from './style';
+import { useEffect, useState } from "react";
+import * as S from "./style";
+import { theme } from "../../../style/theme";
+import Badge from "@components/common/Badge";
 
 interface Room {
-  id: number;
-  title: string;
-  description: string;
-  maxHeadCount: number;
-  currentHeadCount: number;
-  createdAt: string;
+    id: number;
+    title: string;
+    description: string;
+    currentHeadCount: number;
+    isOnline: boolean;
+    createdAt: string;
 }
 
 export default function HomeRoomList(): JSX.Element {
-  // room dummy data
-  const [rooms, setRooms] = useState<Room[]>([]);
+    // room dummy data
+    const [rooms, setRooms] = useState<Room[]>([]);
 
-  const ROOM_DUMMY_DATA: Room[] = [
-    {
-      id: 1,
-      title: '방 제목1',
-      description: '방 설명1',
-      maxHeadCount: 4,
-      currentHeadCount: 2,
-      createdAt: '2021-08-01',
-    },
-    {
-      id: 2,
-      title: '방 제목2',
-      description: '방 설명2',
-      maxHeadCount: 4,
-      currentHeadCount: 3,
-      createdAt: '2021-08-02',
-    },
-    {
-      id: 3,
-      title: '방 제목3',
-      description: '방 설명3',
-      maxHeadCount: 4,
-      currentHeadCount: 4,
-      createdAt: '2021-08-03',
-    },
-    {
-      id: 4,
-      title: '방 제목3',
-      description: '방 설명3',
-      maxHeadCount: 4,
-      currentHeadCount: 4,
-      createdAt: '2021-08-03',
-    },
-    {
-      id: 5,
-      title: '방 제목3',
-      description: '방 설명3',
-      maxHeadCount: 4,
-      currentHeadCount: 4,
-      createdAt: '2021-08-03',
-    },
-    {
-      id: 6,
-      title: '방 제목3',
-      description: '방 설명3',
-      maxHeadCount: 4,
-      currentHeadCount: 4,
-      createdAt: '2021-08-03',
-    },
-    {
-      id: 7,
-      title: '방 제목3',
-      description: '방 설명3',
-      maxHeadCount: 4,
-      currentHeadCount: 4,
-      createdAt: '2021-08-03',
-    },
-    {
-      id: 8,
-      title: '방 제목3',
-      description: '방 설명3',
-      maxHeadCount: 4,
-      currentHeadCount: 4,
-      createdAt: '2021-08-03',
-    },
-  ];
+    const ROOM_DUMMY_DATA: Room[] = [
+        {
+            id: 1,
+            title: "객체지향 프로그래밍",
+            description: "방 설명1",
+            currentHeadCount: 2,
+            isOnline: true,
+            createdAt: "2021-08-01",
+        },
+        {
+            id: 2,
+            title: "방 제목2",
+            description: "방 설명2",
+            currentHeadCount: 3,
+            isOnline: false,
+            createdAt: "2021-08-02",
+        },
+        {
+            id: 3,
+            title: "방 제목3",
+            description: "방 설명3",
+            currentHeadCount: 4,
+            isOnline: true,
+            createdAt: "2021-08-03",
+        },
+        {
+            id: 4,
+            title: "방 제목3",
+            description: "방 설명3",
+            currentHeadCount: 4,
+            isOnline: true,
+            createdAt: "2021-08-03",
+        },
+        {
+            id: 5,
+            title: "방 제목3",
+            description: "방 설명3",
+            currentHeadCount: 4,
+            isOnline: true,
+            createdAt: "2021-08-03",
+        },
+        {
+            id: 6,
+            title: "방 제목3",
+            description: "방 설명3",
+            currentHeadCount: 4,
+            isOnline: true,
+            createdAt: "2021-08-03",
+        },
+        {
+            id: 7,
+            title: "방 제목3",
+            description: "방 설명3",
+            currentHeadCount: 4,
+            isOnline: true,
+            createdAt: "2021-08-03",
+        },
+        {
+            id: 8,
+            title: "방 제목3",
+            description: "방 설명3",
+            currentHeadCount: 4,
+            isOnline: true,
+            createdAt: "2021-08-03",
+        },
+    ];
 
-  useEffect(() => {
-    setRooms(ROOM_DUMMY_DATA);
-  }, []);
+    useEffect(() => {
+        setRooms(ROOM_DUMMY_DATA);
+    }, []);
 
-  return (
-    <S.HomeRoomListWrapper>
-      {rooms.map((room) => (
-        <S.HomeRoomListItem key={room.id}>
-          <S.HomeRoomListItemTitle>{room.title}</S.HomeRoomListItemTitle>
-          <S.HomeRoomListItemDescription>
-            {room.description}
-          </S.HomeRoomListItemDescription>
-          <S.HomeRoomListItemHeadCount>
-            {room.currentHeadCount} / {room.maxHeadCount}
-          </S.HomeRoomListItemHeadCount>
-          <S.HomeRoomListItemCreatedAt>
-            {room.createdAt}
-          </S.HomeRoomListItemCreatedAt>
-        </S.HomeRoomListItem>
-      ))}
-    </S.HomeRoomListWrapper>
-  );
+    return (
+        <S.HomeRoomListWrapper>
+            {rooms.length === 0 ? (
+                <S.HomeRoomListEmpty>
+                    참여한 나루터가 없습니다...
+                </S.HomeRoomListEmpty>
+            ) : (
+                rooms.map((room) => (
+                    <S.HomeRoomListItem key={room.id} isOnline={room.isOnline}>
+                        <S.HomeRoomTitleWrapper>
+                            <S.HomeRoomListIcon />
+                            <S.HomeRoomListItemTitle>
+                                {room.title}
+                            </S.HomeRoomListItemTitle>
+                        </S.HomeRoomTitleWrapper>
+                        <S.HomeRoomListItemDescription>
+                            {room.isOnline ? (
+                                <Badge
+                                    text="관리자 온라인"
+                                    color={theme.Colors.primary}
+                                    textColor={theme.Colors.white}
+                                />
+                            ) : (
+                                <Badge
+                                    text="관리자 오프라인"
+                                    color={theme.Colors.neutral300}
+                                    textColor={theme.Colors.neutral}
+                                />
+                            )}
+                        </S.HomeRoomListItemDescription>
+                        <S.HomeRoomFooter>
+                            <S.HomeRoomListItemHeadCount>
+                                현재 {room.currentHeadCount}명 참여중
+                            </S.HomeRoomListItemHeadCount>
+                            <S.HomeRoomListItemCreatedAt>
+                                참여 날짜: {room.createdAt}
+                            </S.HomeRoomListItemCreatedAt>
+                        </S.HomeRoomFooter>
+                    </S.HomeRoomListItem>
+                ))
+            )}
+        </S.HomeRoomListWrapper>
+    );
 }
--- src/components/home/homeRoomList/style.ts
+++ src/components/home/homeRoomList/style.ts
@@ -1,58 +1,99 @@
-import { styled } from 'styled-components';
+import { FaRadiation } from "react-icons/fa";
+import { theme } from "../../../style/theme";
+import { styled } from "styled-components";
+
+export const HomeRoomTitleWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    margin-bottom: 0.5rem;
+`;
+
+// icon
+export const HomeRoomListIcon = styled(FaRadiation)`
+    font-size: 1rem;
+    margin-right: 1rem;
+`;
 
 export const HomeRoomListWrapper = styled.div`
-  width: 100%;
-  margin: 2rem 0;
-  display: grid;
+    width: 100%;
+    margin: 2rem 0;
+    display: grid;
 
-  @media (max-width: 768px) {
-    grid-template-rows: 2fr;
-    grid-template-columns: 1fr 1fr;
-  }
-  @media (min-width: 768px) {
-    grid-template-rows: 3fr;
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-  @media (min-width: 992px) {
-    grid-template-rows: 4fr;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-  }
-  grid-gap: 4rem 2rem;
+    @media (max-width: 768px) {
+        grid-template-rows: 2fr;
+        grid-template-columns: 1fr 1fr;
+    }
+    @media (min-width: 768px) {
+        grid-template-rows: 3fr;
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    @media (min-width: 992px) {
+        grid-template-rows: 4fr;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+    grid-gap: 4rem 2rem;
 `;
 
 export const HomeRooms = styled.div`
-  border: 1px solid #e0e0e0;
-  border-radius: 8px;
-  padding: 1rem;
-  cursor: pointer;
-  transition: 0.3s;
+    border: 1px solid #e0e0e0;
+    border-radius: 8px;
+    padding: 1rem;
+    cursor: pointer;
+    transition: 0.3s;
 `;
 
-export const HomeRoomListItem = styled.div`
-  border: 1px solid red;
-  padding: 1rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
-`;
+interface HomeRoomListItemProps {
+    isOnline: boolean;
+}
 
-export const HomeRoomListItemTitle = styled.h3`
-  margin: 0;
-  font-size: 1.5rem;
+export const HomeRoomListItem = styled.div<HomeRoomListItemProps>`
+    max-width: 435px;
+    max-height: 156px;
+    padding: 20px;
+    border: 1px solid
+        ${(props) =>
+            props.isOnline ? theme.Colors.primary : theme.Colors.neutral300};
+    border-radius: 8px;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    box-shadow: 2px 12px 12px rgba(0, 0, 0, 0.1);
+`;
+export const HomeRoomListItemTitle = styled.h2`
+    ${theme.Fonts.H2}
 `;
 
 export const HomeRoomListItemDescription = styled.p`
-  margin: 0;
-  font-size: 1rem;
+    margin-bottom: 2rem;
 `;
 
 export const HomeRoomListItemHeadCount = styled.p`
-  margin: 0;
-  font-size: 1rem;
+    ${theme.Fonts.Sub3}
+    color:${theme.Colors.neutral};
 `;
 
 export const HomeRoomListItemCreatedAt = styled.p`
-  margin: 0;
-  font-size: 1rem;
+    ${theme.Fonts.Sub3}
+    color:${theme.Colors.neutral};
+`;
+
+export const HomeRoomListEmpty = styled.div`
+    width: 435px;
+    height: 156px;
+    border: 1px solid ${theme.Colors.neutral};
+    border-radius: 8px;
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${theme.Colors.neutral};
+    ${theme.Fonts.Sub2}
+    box-shadow: 2px 12px 12px rgba(0, 0, 0, 0.1);
+`;
+
+export const HomeRoomFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
 `;
