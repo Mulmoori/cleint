1a639db715806f18c1862b0df8bd4ee46167b2df
bunju20 <ghkd2271@naver.com>
2024-08-19 20:59:22 +0900

    feat: TabBar 구현

--- src/components/layouts/style.ts
+++ src/components/layouts/style.ts
@@ -1,69 +1,113 @@
-// style.ts
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const NavbarWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
-  height: 60px;
-  background-color: #fff;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  position: relative;
-  z-index: 1;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 60px;
+	height: 128px;
+	background-color: #fff;
+	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+	border-bottom: 1.333px rgba(195, 212, 233, 0.4);
+`;
+
+export const LeftSection = styled.div`
+	flex: 1;
+`;
+
+export const CenterSection = styled.div`
+	flex: 1;
+	display: flex;
+	justify-content: center;
+`;
+
+export const RightSection = styled.div`
+	flex: 1;
+	display: flex;
+	justify-content: flex-end;
+`;
+
+export const LogoContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
 export const Logo = styled.div`
-  font-size: 24px;
-  font-weight: bold;
+	width: 160.449px;
+	height: 51.9px;
+	margin-bottom: 8px;
+	img {
+		width: 100%;
+		height: 100%;
+	}
+`;
+
+export const LogoText = styled.div`
+	color: #000;
+	text-align: center;
+	font-family: Pretendard, sans-serif;
+	font-size: 16px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 20px;
+`;
+
+export const IconContainer = styled.div`
+	display: flex;
+	align-items: center;
+	img {
+		width: 58.667px;
+		height: 58.667px;
+	}
 `;
 
 export const HamburgerIcon = styled.div`
-  display: none;
-  font-size: 24px;
-  cursor: pointer;
+	display: none;
+	font-size: 24px;
+	cursor: pointer;
 
-  @media (max-width: 768px) {
-    display: block;
-  }
+	@media (max-width: 768px) {
+		display: block;
+	}
 `;
 
 interface MenuProps {
-  isOpen: boolean;
+	isOpen: boolean;
 }
 
 export const Menu = styled.ul<MenuProps>`
-  display: flex;
-  list-style: none;
-  margin: 0;
-  padding: 0;
+	display: flex;
+	list-style: none;
+	margin: 0;
+	padding: 0;
 
-  @media (max-width: 768px) {
-    display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
-    position: absolute;
-    top: 60px;
-    right: 0;
-    background-color: white;
-    width: 100%;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-    border-radius: 8px;
-  }
+	@media (max-width: 768px) {
+		display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+		position: absolute;
+		top: 60px;
+		right: 0;
+		background-color: white;
+		width: 100%;
+		box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+		border-radius: 8px;
+	}
 `;
 
 export const MenuItem = styled.li`
-  padding: 10px 20px;
-  cursor: pointer;
+	padding: 10px 20px;
+	cursor: pointer;
 
-  &:hover {
-    background-color: #f1f1f1;
-  }
+	&:hover {
+		background-color: #f1f1f1;
+	}
 
-  @media (max-width: 768px) {
-    padding: 15px 20px;
-    border-bottom: 1px solid #eaeaea;
+	@media (max-width: 768px) {
+		padding: 15px 20px;
+		border-bottom: 1px solid #eaeaea;
 
-    &:last-child {
-      border-bottom: none;
-    }
-  }
+		&:last-child {
+			border-bottom: none;
+		}
+	}
 `;
--- src/router.tsx
+++ src/router.tsx
@@ -1,38 +1,40 @@
 // Router.tsx
-import React from 'react';
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
-import { styled } from 'styled-components';
-import Home from './pages/home/Home';
-import Navbar from './components/layouts/Navbart';
+import React from "react";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { styled } from "styled-components";
+import Home from "./pages/home/Home";
+import Navbar from "./components/layouts/Navbart";
+import Profile from "@pages/profile/Profile";
 
 const Wrapper = styled.div`
-  margin: 0 auto;
-  min-height: 100vh;
-  background-color: white;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-  display: flex;
-  flex-direction: column;
+	margin: 0 auto;
+	min-height: 100vh;
+	background-color: white;
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+	display: flex;
+	flex-direction: column;
 `;
 
 const Layout: React.FC = () => {
-  return (
-    <>
-      <Wrapper>
-        <Navbar />
-        <Outlet />
-      </Wrapper>
-    </>
-  );
+	return (
+		<>
+			<Wrapper>
+				<Navbar />
+				<Outlet />
+			</Wrapper>
+		</>
+	);
 };
 
 export default function Router(): JSX.Element {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index path="/" element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index path="/" element={<Home />} />
+					<Route path="/profile" element={<Profile />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
